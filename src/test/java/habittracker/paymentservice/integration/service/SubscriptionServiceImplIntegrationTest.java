3564e6d4fba33f2ed759f87a5ecd5adda9255b48
Илман Богданов <ilman.bogdanov@gmail.com>
2025-02-11 07:42:26 +0300

    FIx bugs with tests and datasource

--- src/test/java/habittracker/paymentservice/integration/service/SubscriptionServiceImplIntegrationTest.java
+++ src/test/java/habittracker/paymentservice/integration/service/SubscriptionServiceImplIntegrationTest.java
@@ -3,21 +3,37 @@ package habittracker.paymentservice.integration.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import habittracker.paymentservice.controller.PlanController;
 import habittracker.paymentservice.model.dto.PlanRequestDTO;
+import io.github.cdimascio.dotenv.Dotenv;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 
 
+import java.io.File;
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class SubscriptionServiceImplIntegrationTest {
 
     @Autowired
     private PlanController planController;
+
+    @BeforeAll
+    static void loadEnv() {
+        // Загружаем переменные окружения
+        String dotenvPath = new File(System.getProperty("user.dir")).getPath();
+        Dotenv dotenv = Dotenv.configure()
+                .directory(dotenvPath)
+                .filename(".env.local")
+                .load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+    }
 
     @Test
     void createPlanAndSubscriptionViaController() {
